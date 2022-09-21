9404b9f32ca0fd605927b10f0d001b86b7dc9060
Jack <johnemmanuelbond@gmail.com>
2022-09-21 15:20:44 -0400

    continued working on aesthetics, re-added batch functionality

--- DensityChargeCorrelationPlotter.py
+++ DensityChargeCorrelationPlotter.py
@@ -23,80 +23,80 @@ ax.set_title("")
 ax.set_xlabel("Topological Charge")
 ax.set_ylabel(r"Local Density [$(2a)^{-2}$]")
 
-#single = len(sys.argv) == 1 or sys.argv[1] != 'batch'
-#batch = sys.argv[1]=='batch'
+single = len(sys.argv) == 1 or sys.argv[1] != 'batch'
+batch = sys.argv[1]=='batch'
 
-#if single:
+if single:
 
-simFolder = os.getcwd()
-config = json.load(open(simFolder + "/configFile.json",'r'))
-simarg = config['simargument']
-params = config['params']
-N = simarg['npart']
-R = simarg['radius']
-a = params["particle_radius"]
-aeff = getAEff(params)
-eta_eff = N*(aeff/(2*a))**2/(4*R**2)
+	simFolder = os.getcwd()
+	config = json.load(open(simFolder + "/configFile.json",'r'))
+	simarg = config['simargument']
+	params = config['params']
+	N = simarg['npart']
+	R = simarg['radius']
+	a = params["particle_radius"]
+	aeff = getAEff(params)
+	eta_eff = N*(aeff/(2*a))**2/(4*R**2)
 
-lab = f"eta_eff={eta_eff:.3f},R={R:.2f},N={N}"
-pltlab = rf"$\eta_{{eff}}$={eta_eff:.3f},R={R:.2f},N={N}"
+	lab = f"eta_eff={eta_eff:.3f},R={R:.2f},N={N}"
+	pltlab = rf"$\eta_{{eff}}$={eta_eff:.3f},R={R:.2f},N={N}"
 
-frames = np.array(sample_frames([simFolder+"/"],label=lab,reset=True))
+	frames = np.array(sample_frames([simFolder+"/"],label=lab,reset=True))
 
-qs = np.array([6-Vc(frame, R = R) for frame in frames]).flatten()
-#XS = 0.5*(np.array([np.sum(np.abs(q)) for q in qs])/12-1)
-rhos = np.array([rho_voronoi(frame,R=R) for frame in frames]).flatten()
+	qs = np.array([6-Vc(frame, R = R) for frame in frames]).flatten()
+	#XS = 0.5*(np.array([np.sum(np.abs(q)) for q in qs])/12-1)
+	rhos = np.array([rho_voronoi(frame,R=R) for frame in frames]).flatten()
 
-pltqs=[]
-pltrhos=[]
-pltdrhos=[]
+	pltqs=[]
+	pltrhos=[]
+	pltdrhos=[]
 
-for q in np.unique(qs):
-	rho = rhos[qs==q].mean()
-	drho = rhos[qs==q].std()
+	for q in np.unique(qs):
+		rho = rhos[qs==q].mean()
+		drho = rhos[qs==q].std()
 
-	pltqs.append(q)
-	pltrhos.append(rho)
-	pltdrhos.append(drho)
+		pltqs.append(q)
+		pltrhos.append(rho)
+		pltdrhos.append(drho)
 
-ax.errorbar(pltqs,pltrhos,yerr=pltdrhos,label=pltlab, ls='none', ms='H', facecolors='none')
+	ax.errorbar(pltqs,pltrhos,yerr=pltdrhos,label=pltlab, ls='none', ms='H', facecolors='none')
 
-# elif batch:
-# 	configs, seedFoldersList = categorize_batch()
+elif batch:
+	configs, seedFoldersList = categorize_batch()
 
-# 	for i,seedFolders in enumerate(seedFoldersList):
-# 		simarg = configs[i]['simargument']
-# 		params = configs[i]['params']
-# 		N = simarg['npart']
-# 		R = simarg['radius']
-# 		a = params["particle_radius"]
-# 		aeff = getAEff(params)
-# 		eta_eff = N*(aeff/(2*a))**2/(4*R**2)
+	for i,seedFolders in enumerate(seedFoldersList):
+		simarg = configs[i]['simargument']
+		params = configs[i]['params']
+		N = simarg['npart']
+		R = simarg['radius']
+		a = params["particle_radius"]
+		aeff = getAEff(params)
+		eta_eff = N*(aeff/(2*a))**2/(4*R**2)
 
-# 		lab = f"eta_eff={eta_eff:.3f},R={R:.2f},N={N}"
-# 		pltlab = rf"$\eta_{{eff}}$={eta_eff:.3f},R={R:.2f},N={N}"
+		lab = f"eta_eff={eta_eff:.3f},R={R:.2f},N={N}"
+		pltlab = rf"$\eta_{{eff}}$={eta_eff:.3f},R={R:.2f},N={N}"
 
-# 		frames = np.array(sample_frames(seedFolders,label=lab,reset=True))
+		frames = np.array(sample_frames(seedFolders,label=lab,reset=True))
 
-# 		qs = np.array([6-Vc(frame, R = R) for frame in frames]).flatten()
-# 		#XS = 0.5*(np.array([np.sum(np.abs(q)) for q in qs])/12-1)
-# 		rhos = np.rray([rho_voronoi(frame,R=R) for frame in frames]).flatten()
+		qs = np.array([6-Vc(frame, R = R) for frame in frames]).flatten()
+		#XS = 0.5*(np.array([np.sum(np.abs(q)) for q in qs])/12-1)
+		rhos = np.rray([rho_voronoi(frame,R=R) for frame in frames]).flatten()
 
-# 		pltqs=[]
-# 		pltrhos=[]
-# 		pltdrhos=[]
+		pltqs=[]
+		pltrhos=[]
+		pltdrhos=[]
 
-# 		for q in np.unique(qs):
-# 			rho = rhos[qs==q].mean()
-# 			drho = rhos[qs==q].std()
+		for q in np.unique(qs):
+			rho = rhos[qs==q].mean()
+			drho = rhos[qs==q].std()
 
-# 			pltqs.append(q)
-# 			pltrhos.append(rho)
-# 			pltdrhos.append(pltdrhos)
+			pltqs.append(q)
+			pltrhos.append(rho)
+			pltdrhos.append(drho)
 
-# 		ax.errorbar(pltqs,pltrhos,yerr=pltdrhos,label=pltlab)
+		ax.errorbar(pltqs,pltrhos,yerr=pltdrhos,label=pltlab)
 
-ax.legend(loc = 5)
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 fig.savefig("DensityChargeCorrelation.jpg")
 
