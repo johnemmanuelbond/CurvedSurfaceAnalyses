291854dfb1a11d0c6ec38b0ee405864284fc5a38
Jack <johnemmanuelbond@gmail.com>
2022-09-29 14:34:33 -0400

    typo

--- lammpsScarAnalysis.py
+++ lammpsScarAnalysis.py
@@ -72,8 +72,8 @@ scarTotalCharges = []
 
 #loop through equilibrated frames to gather information about charged clusters. We don't want to excessively calculate cluster positions, and we only want to sample independent frames, so I've created an array of index positions to control which frames we actually perform calculations on.
 last_section = 1/3
-desired_samples = 50
-idx = np.arange(int((1-last_section)*fnum),fnum,(last_section*fnum)/samples)
+desired_samples = 100
+idx = np.arange(int((1-last_section)*fnum),fnum,(last_section*fnum)/desired_samples)
 
 for i in idx:
 	frame = multiple[i]
