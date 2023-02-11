a008a357304be9ba0918142045e2879458949cf3
Jack <johnemmanuelbond@gmail.com>
2023-02-10 19:20:30 -0500

    tryna make plots visible

--- lammpsMSDPlotter.py
+++ lammpsMSDPlotter.py
@@ -212,13 +212,13 @@ if __name__=="__main__":
     fig, ax = plt.subplots(figsize=(5,5))
     ax.plot(msd_times, msd, label='ensemble mto msd')
     
-    subtends = np.array([np.pi/10,theta1/2,np.pi/4,theta1,theta2/2,np.pi/2])
+    subtends = np.array([np.pi/10,theta1/2,np.pi/4,theta2/2,np.pi/2])
 
     for i, subtend in enumerate(subtends):
         msd_com, msd_rad, mean_n, c_vec = mto_com_sector_msd(multiple,msd_time_scale,skips=s, theta_c = (theta_c_1+np.pi/2) % np.pi, phi_c = phi_c_1,subtended_halfangle = subtend)
         
         ax.plot(msd_times,msd_com.sum(axis=-1), label = f"com msd about {np.round(c_vec,2)}\nsubtended angle: {2*subtend/np.pi:.2f}$\pi$ rad", color=f"C{i+1}",lw=0.8)
-        ax.plot(msd_times,msd_rad, label = f"radial com msd about {np.round(c_vec,2)}\nsubtended angle: {2*subtend/np.pi:.2f}$\pi$ rad", color=f"C{i+1}",lw=0.6, ls="-.")
+        ax.plot(msd_times,msd_rad, label = f"radial com msd about {np.round(c_vec,2)}\n(~{mean_n:.1f} ptcls)", color=f"C{i+1}",lw=0.6, ls="-.")
 
     ax.set_xlabel("[$\\tau$]", fontsize=12)
     ax.set_xlim([0, msd_times[-1]])
