9a95ba8f7bd2d4f4d44e352cf7f5b4cdb4189a91
Jack <johnemmanuelbond@gmail.com>
2023-03-15 15:51:52 -0400

    changed find_DL to locate the first time instantaneous diffusivity increases as an estimator for when long-time diffusion begins.

--- main_lib/MSD.py
+++ main_lib/MSD.py
@@ -506,8 +506,8 @@ def find_DL(lagtime, msd, dim=2, window=100):
     Ds = np.array(Ds)
     dDs = np.array(dDs)
     ts = np.array(ts)
-    D_diff = np.abs(Ds[1:]-Ds[:-1])
-    i_cross = np.argmax(D_diff)
+    D_diff = Ds[1:]-Ds[:-1]
+    i_cross = np.where(D_diff>0)[0]
 
     return ts, Ds, dDs, i_cross
 
